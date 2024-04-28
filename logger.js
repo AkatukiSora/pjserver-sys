a5aa55e63455985ae607f33e09017cb392257b17
saikyounosennsi <sato18782@gmail.com>
2024-04-28 19:52:42 +0900

    loggerの設定ファイル作成

---
+++ logger.js
@@ -0,0 +1,41 @@
+log4js = require('log4js')
+
+log4js.configure({
+  appenders: {
+    out: {
+      type: 'stdout',
+      layout: { type: 'colored' }
+    },
+    logFile: { 
+      type: 'dateFile',
+      filename: 'logs/log/log.log',
+      pattern: '.yyyy-MM-dd',
+      numBuckups: 30,
+      compress: true,
+      keepFileExt: true,
+    },
+    errFile: { 
+      type: 'dateFile',
+      filename: 'logs/err/err.log',
+      pattern: '.yyyy-MM-dd',
+      numBuckups: 30,
+      compress: true,
+      keepFileExt: true,
+    },
+    flog: {
+      type: 'logLevelFilter',
+      appender: ['logFile'],
+      level: 'info',
+    },
+    ferr: {
+      type: 'logLevelFilter',
+      appender: ['errFile'],
+      level: 'warn',
+    },
+  },
+  categories: {
+    default: { appenders: ['out', 'flog', 'ferr'], level: 'all' }
+  }
+})
+
+module.exports = log4js.getLogger()
